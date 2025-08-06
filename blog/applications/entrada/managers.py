36f41719c59c1587f781618270fe6dd572f47152
Santiago <santiagomendieta2698@gmail.com>
2025-08-06 08:18:48 -0500

    Nuevo commit

--- blog/applications/entrada/managers.py
+++ blog/applications/entrada/managers.py
@@ -3,7 +3,21 @@ from django.db import models
 class EntryManager(models.Manager):
     """Procedimientos para entrada"""
     def entrada_en_portada(self):
+        # Devuelve la ultima portada
         return self.filter(
             public=True,
             portada=True,
         ).order_by('-created').first()
+    def entradas_en_home(self):
+        # Devuelve las ultimas 4 entradas en home
+        return self.filter(
+            public=True,
+            in_home=True,
+        ).order_by('-created')[:4]
+    def entradas_recientes(self):
+        # Devuelve las ultimas 6 entradas recientes
+        return self.filter(
+            public=True,
+        ).order_by('-created')[:6]
+
+
